a65413b146fd2974fe61f108c2e0595ec4f2c457
Eroc <hostiaurelio@gmail.com>
2018-09-25 19:33:03 +0200

    nuuuuub

---
+++ src/main/java/losjuquers/AD02_MostraContingutFitxerText/App.java
@@ -0,0 +1,13 @@
+package losjuquers.AD02_MostraContingutFitxerText;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
